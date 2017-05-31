c886ab73fb340fe88288c7a60c43f1fed59f9d21
DESKTOP-L61HIB8\tiger <tigerbarras@msn.com>
2017-05-30 20:18:22 -0600

    Implements function (generateTransferValue) to generate output value from an array.
    Removed turnAllOn(), as it is redundant with ledDriver.setState()
    
    Changed the test code in setup() and loop()

--- lightsout/lightsout.ino
+++ lightsout/lightsout.ino
@@ -1,28 +1,32 @@
 // Include the SPI library
 #include <SPI.h>
 
-#include "STP16CP26.h" // Include library for using the LED driver
+// Include library for using the LED driver
+#include "STP16CP26.h" 
+
+// Pins for slider switch at top
 #define SW1 2
 #define SW2 3
 #define SW3 4
 #define SW4 5
+
+// Pins for push buttons along side
 #define push1 A1
 #define push2 A2
 #define push3 A3
 #define push4 A4
 
-const int OE = 9;
-const int chipSelectPin = 10;
+#define OE 9
+#define CHIPSELECTPIN 10
 
+#define LEDARRAYHEIGHT  4
+#define LEDARRAYWIDTH 4
 
-
-const short ledArrayHeight = 4;
-const short ledArrayWidth  = 4;
-boolean ledArray[ledArrayHeight][ledArrayWidth];
+boolean ledArray[LEDARRAYHEIGHT][LEDARRAYWIDTH];
 
 SPISettings spiSettings(14000000,MSBFIRST,SPI_MODE0); // instantiate spiSettings
 
-STP16CP26 ledDriver(OE,chipSelectPin); // instantiate object ledDrive of class STP16CP26
+STP16CP26 ledDriver(OE,CHIPSELECTPIN); // instantiate object ledDrive of class STP16CP26
 /* 
  * Set each LED in the array to be either high or low
  * Input:
@@ -33,70 +37,69 @@ STP16CP26 ledDriver(OE,chipSelectPin); // instantiate object ledDrive of class S
  * Does not actually turn the LEDs on or off
  * Call UpdateLedArray afterwards to actually turn on or off the LEDS
  */
-void randomizeLedArray(boolean leds[ledArrayHeight][ledArrayWidth]){
-  for(short i = 0; i < ledArrayHeight; i++){
-    for(short k = 0; k < ledArrayWidth; k++){
-      //leds[i][k] = random(10)%2;
-      leds[i][k] = 1; // TESTING ONLY
+void randomizeLedArray(boolean leds[LEDARRAYHEIGHT][LEDARRAYWIDTH]){
+  for(short i = 0; i < LEDARRAYHEIGHT; i++){
+    for(short k = 0; k < LEDARRAYWIDTH; k++){
+      leds[i][k] = random(10)%2;
     }
   }
 }
 
-// Update LEDs to match array
-void updateLedArray(boolean leds, const short height, const short width){
+// Generate a transfer value from the array.
+// Returns transfer value
+uint16_t generateTransferValue(boolean leds[LEDARRAYHEIGHT][LEDARRAYWIDTH]){
+  // The controller sees the leds as a linear row
+  // count denotes which led we're at
+  // Then we can shift the value for that LED into the proper position in the transfer value
+  short count = 0;
+  uint16_t transferValue = 0;
 
+  // Build transfer value
+  // A (1) in a position indicates that led should be turned on
+  for(short height = 0; height < 4; height++){
+    for(short width = 0; width < 4; width++){
+      // If we want the led to be turned on
+      // Shift a 1 into that position in the transfer value
+      if(leds[height][width] == 1){
+        transferValue += (1 << count);
+      }
+
+      count++;
+    }
+  }
+
+  return transferValue;
 }
 
 
-void turnAllOn(void){
+//------------------------------------------------------------------------------------------------------------------
 
- SPI.beginTransaction(spiSettings);
-//  SPI.begin();
- digitalWrite(OE,HIGH);
- digitalWrite(chipSelectPin,LOW);
- SPI.transfer16(0b1001010111111001);
- digitalWrite(chipSelectPin,HIGH);
- SPI.endTransaction();
-  delay(10);
- digitalWrite(chipSelectPin,LOW);
-
- digitalWrite(OE,LOW);
-
-}
 void setup() {
-
-  
-  pinMode(chipSelectPin,OUTPUT);
-  pinMode(13, OUTPUT); // SCK set to output
-  pinMode(11,OUTPUT);  // MOSI set to output
   Serial.begin(9600); // initialize serial at 9600 baud
   Serial.println("Setup...");
+  
+  pinMode(CHIPSELECTPIN,OUTPUT);
   pinMode(OE, OUTPUT );
-
-  // Set up two bytes to transfer
-  // Each bit represents one led
-  byte b0 = B10011100;
-  byte b1 = B01000010;
-  uint16_t b = b1 << 8;
-  b += b0;
-  b = 0xA5A5; // random for testing 
-  // Take the chipSelectPin pin low to select the chip
-
+  pinMode(13, OUTPUT); // SCK set to output
+  pinMode(11,OUTPUT);  // MOSI set to output
+  
   Serial.println("Setup complete...");
-  ledDriver.init(0xFFFF);
+  ledDriver.init(0b0101010101010101);
+
+  
+  // Delay 2 seconds after setup
+  delay(2000);
 }
 
-// instantiate switch state
-
-
 void loop() {
- 
- // turnAllOn();
 
-  ledDriver.setState(0xF3A2);
-  delay(2000);
-  ledDriver.setState(0xFFFF);
+  // Test code. Flashes a random pattern and then a test pattern.
+  randomizeLedArray(ledArray);
+  ledDriver.setState(generateTransferValue(ledArray));
   delay(1000);
+  ledDriver.setState(0b1010101010101010);
+  delay(1000);
+  
 
  // check status of switches
  Serial.print("SW1 = "); Serial.print(digitalRead(SW1)); Serial.print(" SW2 = "); Serial.print(digitalRead(SW2)); Serial.print(" SW3 = "); Serial.print(digitalRead(SW3)); Serial.print(" SW4 = "); Serial.print(digitalRead(SW4));Serial.println();
